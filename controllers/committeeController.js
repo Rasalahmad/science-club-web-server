147bd858387d95f07f01bea0c499d9109f8ff4c0
Rasalahmad <rasalahmadsit@gmail.com>
2023-01-29 00:02:48 +0600

    wip

--- controllers/committeeController.js
+++ controllers/committeeController.js
@@ -5,10 +5,6 @@ function postCommittee(req, res, next) {
   console.log(committee);
   res.status(200).json({ status: true, message: "Data uploaded successfully" });
 }
-function getCommittee(req, res, next) {
-  // const committee = new Committee(req.body);
-  // console.log(committee);
-  // res.status(200).json({ status: true, message: "Data uploaded successfully" });
-}
+function getCommittee(req, res, next) {}
 
 export { postCommittee, getCommittee };
