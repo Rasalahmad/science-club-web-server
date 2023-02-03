be09731d21738e8865f16cad5877500b2f3e37b1
Rasalahmad <rasalahmadsit@gmail.com>
2023-02-03 20:17:46 +0600

    wip

--- controllers/noticeController.js
+++ controllers/noticeController.js
@@ -1,38 +1,81 @@
 import Notice from "../modules/noticeModel";
 
 export const postNotice = async (req, res, next) => {
-  const notice = new Notice({ ...req.body, image: req?.files[0]?.filename });
-  notice.save();
-  res.status(200).json({
-    status: true,
-    message: "Notice published successfully",
-  });
+  try {
+    const notice = new Notice({ ...req.body, image: req?.files[0]?.filename });
+    notice.save();
+    res.status(200).json({
+      status: true,
+      message: "Notice published successfully",
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      let errors = {};
+      Object.keys(error.errors).forEach((key) => {
+        errors[key] = error.errors[key].message;
+      });
+      return res.status(400).json(errors);
+    }
+    res.status(500).json("Something went wrong");
+  }
 };
 export const getNotice = async (req, res, next) => {
-  const notice = await Notice.find({});
-  res.status(200).json({
-    status: true,
-    message: "Notice fetch successfully",
-    data: notice,
-  });
+  try {
+    const notice = await Notice.find({});
+    res.status(200).json({
+      status: true,
+      message: "Notice fetch successfully",
+      data: notice,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      let errors = {};
+      Object.keys(error.errors).forEach((key) => {
+        errors[key] = error.errors[key].message;
+      });
+      return res.status(400).json(errors);
+    }
+    res.status(500).json("Something went wrong");
+  }
 };
 export const updateNotice = async (req, res, next) => {
-  const updated = await Notice.findByIdAndUpdate(
-    req.params.id,
-    { $set: req.body },
-    { new: true }
-  );
-  res.status(200).json({
-    status: true,
-    message: "Notice updated successfully",
-    data: updated,
-  });
+  try {
+    const updated = await Notice.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    res.status(200).json({
+      status: true,
+      message: "Notice updated successfully",
+      data: updated,
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      let errors = {};
+      Object.keys(error.errors).forEach((key) => {
+        errors[key] = error.errors[key].message;
+      });
+      return res.status(400).json(errors);
+    }
+    res.status(500).json("Something went wrong");
+  }
 };
 export const deleteNotice = async (req, res, next) => {
-  console.log(req.params.id);
-  await Notice.findByIdAndDelete(req.params.id);
-  res.status(200).json({
-    status: true,
-    message: "Notice deleted successfully",
-  });
+  try {
+    await Notice.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: true,
+      message: "Notice deleted successfully",
+    });
+  } catch (error) {
+    if (error.name === "ValidationError") {
+      let errors = {};
+      Object.keys(error.errors).forEach((key) => {
+        errors[key] = error.errors[key].message;
+      });
+      return res.status(400).json(errors);
+    }
+    res.status(500).json("Something went wrong");
+  }
 };
